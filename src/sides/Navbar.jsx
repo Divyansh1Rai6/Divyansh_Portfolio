b1f74d7df461412f0686474e3a1e3fc15dace88c
Divyansh1Rai6 <divyansh16rai@gmail.com>
2025-11-24 14:35:47 +0530

    Navbar vertical space fixed

--- src/sides/Navbar.jsx
+++ src/sides/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
         <button onClick={() => setmobileOpen(!mobileOpen)}  className='relative md:hidden text-gray-200'>{mobileOpen ? <X className='w-6 h-6' /> : <Menu className='w-6 h-6' />}</button>
       </div>
       <nav className={`md:hidden w-screen border-t backdrop-blur-sm border-gray-400 transition-all duration-300 ease-in-out overflow-hidden ${mobileOpen ? 'max-h-64 py-4 opacity-100' : 'max-h-0 py-0 opacity-0'}`}>
-        <ul>
+        <ul className='flex flex-col space-y-4 px-4'>
           <li><Link to='/' className='font-bold text-gray-200 hover:text-gray-500'>Home</Link></li>
           <li><Link to='/about' className='font-bold text-gray-200 hover:text-gray-500'>About</Link></li>
           <li><Link to='/skill' className='font-bold text-gray-200 hover:text-gray-500'>Skills</Link></li>
